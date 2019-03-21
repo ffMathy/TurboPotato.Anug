8b10d04e0bc0153afa0415ea0d5b610434dfb69b
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-03-21 10:26:14 +0100

    better example.

---
+++ src/Kibana/Program.cs
@@ -0,0 +1,21 @@
+﻿using FluffySpoon.Kibana;
+using System;
+using System.IO;
+
+namespace Kibana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kibanaUrl = File.ReadAllText("KibanaUrl.txt");
+
+            var parser = new KibanaParser();
+            var elasticsearchQuery = parser.ConvertUrlToElasticsearchQueryString(kibanaUrl, "order_date");
+
+            Console.WriteLine(elasticsearchQuery);
+
+            Console.ReadLine();
+        }
+    }
+}
